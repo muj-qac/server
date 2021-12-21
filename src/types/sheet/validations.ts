00fcd5be0925e90df1f983b125a3db2c8bccfccc
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-12-21 23:26:55 +0530

    add: sheet validations

---
+++ src/types/sheet/validations.ts
@@ -0,0 +1,45 @@
+export interface TextRule {
+  invalidHelpText?: string;
+  equals?: string;
+  notContains?: string;
+  contains?: string;
+  isValidEmail?: boolean;
+  isValidURL?: boolean;
+}
+
+export interface NumberRule {
+  equals?: number;
+  between?: { from: number; to: number };
+  lessThan?: { number: number; equal: boolean };
+  greaterThan?: { number: number; equal: boolean };
+}
+
+export interface NumberRule {
+  equals?: number;
+  between?: { from: number; to: number };
+  lessThan?: { number: number; equal: boolean };
+  greaterThan?: { number: number; equal: boolean };
+}
+
+export interface DateRule {
+  isValid?: boolean;
+  between?: { from: string; to: string };
+  lessThan?: { date: string; equal: boolean };
+  greaterThan?: { date: string; equal: boolean };
+}
+
+export interface DropdownRule {
+  items: string[];
+}
+
+export interface CheckboxRule {
+  useCustom?: boolean;
+  customValues?: { true: string; false: string };
+}
+
+export interface CellValidation {
+  type: 'text' | 'number' | 'date' | 'dropdown' | 'checkbox';
+  invalidAction: 'warn' | 'reject';
+  rule?: TextRule | NumberRule | DateRule | DropdownRule | CheckboxRule;
+  invalidHelpText?: string;
+}
