03bf037eeb774036382ff5526f82fce2b3508f7e
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2022-01-05 19:58:55 +0530

    add: sheet builder with validations

--- src/controllers/builder.controller.ts
+++ src/controllers/builder.controller.ts
@@ -1,31 +1,41 @@
 import { RequestHandler } from 'express';
+
 import { ajv } from '../config/ajv.config';
-import { asyncWrap } from '../middlewares/async.middleware';
-import { throwError } from '../helpers/ErrorHandler.helper';
 
 import { CellValidation } from '../types/sheet/validations';
+import ApiResponse from '../types/api';
+
+import { throwError } from '../helpers/ErrorHandler.helper';
+import { asyncWrap } from '../middlewares/async.middleware';
 import { isValidCellTypeInput, sheetSchemas } from '../validators/sheet';
+import { buildSheet } from '../handlers/googleSheetBuilder.handler';
 
 export const getNewSheetData: RequestHandler = asyncWrap(async (req, res) => {
   try {
     let columns: CellValidation[] = req.body.columns;
 
+    // validates the input json
     columns.forEach((column) => {
-      console.log(column);
+      // check if cell is valid
       ajv.validate(isValidCellTypeInput, column);
-      if (ajv.errors) throw ajv.errors;
+      if (ajv.errors) throwError(400, JSON.stringify(ajv.errors));
 
+      // validate cell rules
       if (column.rule) {
         ajv.validate(sheetSchemas[column.type], column.rule);
-        if (ajv.errors) throw ajv.errors;
+        if (ajv.errors) throwError(400, JSON.stringify(ajv.errors));
       }
     });
 
-    res
-      .status(200)
-      .json({ success: true, response: columns, errors: ajv.errors });
+    const result = await buildSheet(columns);
+    if (!result?.success) throwError(500, 'Error building the sheet.');
+
+    // TODO: generate the sheet download link
+    // TODO: save the details in the db
+
+    res.json({ status: 200, data: result?.data, success: true } as ApiResponse);
   } catch (error) {
     console.log(error);
-    throwError(400, `Some error occurred. ${JSON.stringify(error)}`);
+    throwError(500, `Some error occurred.`);
   }
 });
