214d8a71cb6553d7a99fa7fd40d351f7ec9ad630
ArjunAghera <agheraarjun23@gmail.com>
2022-01-13 23:58:04 +0530

    feat:verify and download kpi

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -1,6 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { RequestHandler } from 'express';
 import { throwError } from '../helpers/ErrorHandler.helper';
 import { asyncWrap } from '../middlewares/async.middleware';
+import aws from 'aws-sdk';
+
+const s3 = new aws.S3({
+  accessKeyId: `${process.env.AWS_ACCESS_KEY}`,
+  secretAccessKey: `${process.env.AWS_SECRET_ACCESS_KEY}`,
+  signatureVersion: 'v4',
+});
+
+let bucketParams = {
+  Bucket: `${process.env.AWS_BUCKET_NAME}`,
+};
+
+let verifiedBucketParams = {
+  Bucket: `${process.env.AWS_BUCKET_NAME_VERIFIED}`,
+};
 
 export const postKPI: RequestHandler<any> = asyncWrap(
   async (req, res, _next) => {
@@ -8,6 +25,82 @@ export const postKPI: RequestHandler<any> = asyncWrap(
       res.status(200).json({
         data: req.file,
         msg: 'Successfully uploaded ' + req.file?.originalname + ' files!',
+      });
+    } catch (error) {
+      console.error(error);
+      throwError(400, 'Some error occurred.');
+    }
+  },
+);
+
+export const getUnverifiedKPIs: RequestHandler<any> = asyncWrap(
+  async (_req, res, _next) => {
+    try {
+      s3.listObjects(bucketParams, function (err, data) {
+        if (err) {
+          res.status(404).json({ Error: err });
+        } else {
+          res.status(200).json({ Success: data });
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      throwError(400, 'Some error occurred.');
+    }
+  },
+);
+
+const getFileStream = (key) => {
+  let fileParams = {
+    Key: key,
+    Bucket: `${process.env.AWS_BUCKET_NAME}`,
+  };
+  return s3.getObject(fileParams).createReadStream();
+};
+
+export const getObject: RequestHandler<any> = asyncWrap(
+  async (_req, res, _next) => {
+    try {
+      const objectKey = _req.body.fileKey;
+      const readStream = getFileStream(objectKey);
+      res.attachment('sheet.xlsx');
+      readStream.pipe(res);
+    } catch (error) {
+      console.error(error);
+      throwError(400, 'Some error occurred.');
+    }
+  },
+);
+
+export const verifyKPI: RequestHandler<any> = asyncWrap(
+  async (_req, res, _next) => {
+    try {
+      const key = _req.body.fileKey;
+      const kpiParams = {
+        Bucket: `${process.env.AWS_BUCKET_NAME_VERIFIED}`,
+        CopySource: '/' + `${process.env.AWS_BUCKET_NAME}` + '/' + key,
+        Key: key,
+      };
+      s3.copyObject(kpiParams, function (err, data) {
+        if (err) res.status(404).json({ err });
+        else res.status(200).json({ data });
+      });
+    } catch (error) {
+      console.error(error);
+      throwError(400, 'Some error occurred.');
+    }
+  },
+);
+
+export const getVerifiedKPIs: RequestHandler<any> = asyncWrap(
+  async (_req, res, _next) => {
+    try {
+      s3.listObjects(verifiedBucketParams, function (err, data) {
+        if (err) {
+          res.status(404).json({ Error: err });
+        } else {
+          res.status(200).json({ Success: data });
+        }
       });
     } catch (error) {
       console.error(error);
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,15 +1,18 @@
-import { Router } from "express";
-import * as user from '../controllers/user.controller'
-
+import { Router } from 'express';
+import * as user from '../controllers/user.controller';
+import { postKPI } from '../controllers/upload.controller';
+import uploadMiddleware from '../middlewares/upload.middleware';
 
 const router = Router();
 
-router.get('/my-profile',user.getMyProfile);
+router.get('/my-profile', user.getMyProfile);
 
 router.get('/', (req, res, _next) => {
   res.send(req.user);
 });
 
 router.put('/change-password');
+
+router.post('/upload/:kpi', uploadMiddleware.single('file'), postKPI);
 
 export default router;
