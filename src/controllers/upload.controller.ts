b65604caa5a41a7c122af0e1a39be257d4bfb296
ArjunAghera <agheraarjun23@gmail.com>
2022-01-17 19:50:08 +0530

    feat: reject kpi

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -3,6 +3,7 @@ dotenv.config();
 import { RequestHandler } from 'express';
 import { throwError } from '../helpers/ErrorHandler.helper';
 import { asyncWrap } from '../middlewares/async.middleware';
+// import { statusTypes, UploadedSheet } from '../models/UploadedSheet.model';
 import aws from 'aws-sdk';
 import xlsx from 'xlsx';
 
@@ -20,12 +21,24 @@ let verifiedBucketParams = {
   Bucket: `${process.env.AWS_BUCKET_NAME_VERIFIED}`,
 };
 
+let rejectedBucketParams = {
+  Bucket: `${process.env.AWS_BUCKET_NAME_REJECTED}`,
+};
+
+const getFileStream = (key, bucket) => {
+  let fileParams = {
+    Key: key,
+    Bucket: bucket,
+  };
+  return s3.getObject(fileParams).createReadStream();
+};
+
 export const postKPI: RequestHandler<any> = asyncWrap(
   async (req, res, _next) => {
     try {
       res.status(200).json({
-        data: req.file,
         msg: 'Successfully uploaded ' + req.file?.originalname + ' files!',
+        data: req.file,
       });
     } catch (error) {
       console.error(error);
@@ -41,7 +54,7 @@ export const getUnverifiedKPIs: RequestHandler<any> = asyncWrap(
         if (err) {
           res.status(404).json({ Error: err });
         } else {
-          res.status(200).json({ Success: data });
+          res.status(200).json({ 'Unverified kpis': data.Contents });
         }
       });
     } catch (error) {
@@ -51,24 +64,44 @@ export const getUnverifiedKPIs: RequestHandler<any> = asyncWrap(
   },
 );
 
-const getFileStream = (key, bucket) => {
-  let fileParams = {
-    Key: key,
-    Bucket: bucket,
-  };
-  return s3.getObject(fileParams).createReadStream();
-};
-
 export const getUnverifiedObject: RequestHandler<any> = asyncWrap(
   async (_req, res, _next) => {
     try {
       const objectKey = _req.body.fileKey;
       const bucket = `${process.env.AWS_BUCKET_NAME}`;
       const readStream = getFileStream(objectKey, bucket);
-      res.attachment('sheet.xlsx');
+      res.attachment(`${objectKey}.xlsx`);
       readStream.pipe(res);
     } catch (error) {
       console.error(error);
+      throwError(400, 'Some error occurred.');
+    }
+  },
+);
+
+export const rejectKPI: RequestHandler<any> = asyncWrap(
+  async (_req, res, _next) => {
+    try {
+      const key = _req.body.fileKey;
+      const kpiParamsToReject = {
+        Bucket: `${process.env.AWS_BUCKET_NAME_REJECTED}`,
+        CopySource: `/${process.env.AWS_BUCKET_NAME}/${key}`,
+        Key: key,
+      };
+      const kpiParams = {
+        Bucket: `${process.env.AWS_BUCKET_NAME}`,
+        Key: key,
+      };
+      s3.copyObject(kpiParamsToReject, function (err, data) {
+        if (err) res.status(400).json({ err });
+        else res.status(200).json({ data });
+      });
+      s3.deleteObject(kpiParams, function (err, data) {
+        if (err) console.log({ err });
+        else console.log({ data });
+      });
+    } catch (error) {
+      console.log(error);
       throwError(400, 'Some error occurred.');
     }
   },
@@ -104,7 +137,7 @@ export const verifyKPI: RequestHandler<any> = asyncWrap(
 
 //! ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 export const updateMainKPI: RequestHandler<any> = asyncWrap(
-  async (_req, res, _next) => {
+  async (_req, _res, _next) => {
     try {
       const key = _req.body.fileKey;
       const bucket = `${process.env.AWS_BUCKET_NAME_VERIFIED}`;
@@ -122,16 +155,18 @@ export const updateMainKPI: RequestHandler<any> = asyncWrap(
         let buffer = Buffer.concat(buffers);
         let verifiedSheetData = xlsx.read(buffer);
         let verifiedSheetJson: any = xlsx.utils.sheet_to_json(
-          verifiedSheetData.Sheets[0],
+          verifiedSheetData.Sheets[verifiedSheetData.SheetNames[0]],
         );
+        const verifiedSheetJsonToSheet: any =
+          xlsx.utils.json_to_sheet(verifiedSheetJson);
+        console.log(verifiedSheetJsonToSheet);
         const modifiedWorkbook = xlsx.utils.book_new();
         let sheet = xlsx.utils.book_append_sheet(
-          verifiedSheetJson,
+          verifiedSheetJsonToSheet,
           modifiedWorkbook,
           'kpiName',
         );
-        console.log(verifiedSheetJson);
-        res.send(sheet);
+        _res.send(sheet);
       });
     } catch (error) {
       console.error(error);
@@ -147,7 +182,24 @@ export const getVerifiedKPIs: RequestHandler<any> = asyncWrap(
         if (err) {
           res.status(404).json({ Error: err });
         } else {
-          res.status(200).json({ Success: data });
+          res.status(200).json({ 'Verified kpis': data.Contents });
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      throwError(400, 'Some error occurred.');
+    }
+  },
+);
+
+export const getRejectedKPIs: RequestHandler<any> = asyncWrap(
+  async (_req, res, _next) => {
+    try {
+      s3.listObjects(rejectedBucketParams, function (err, data) {
+        if (err) {
+          res.status(404).json({ Error: err });
+        } else {
+          res.status(200).json({ 'Rejected kpis': data.Contents });
         }
       });
     } catch (error) {
@@ -163,7 +215,22 @@ export const getVerifiedObject: RequestHandler<any> = asyncWrap(
       const objectKey = _req.body.fileKey;
       const bucket = `${process.env.AWS_BUCKET_NAME_VERIFIED}`;
       const readStream = getFileStream(objectKey, bucket);
-      res.attachment('sheet.xlsx');
+      res.attachment(`${objectKey}.xlsx`);
+      readStream.pipe(res);
+    } catch (error) {
+      console.error(error);
+      throwError(400, 'Some error occurred.');
+    }
+  },
+);
+
+export const getRejectedObject: RequestHandler<any> = asyncWrap(
+  async (_req, res, _next) => {
+    try {
+      const objectKey = _req.body.fileKey;
+      const bucket = `${process.env.AWS_BUCKET_NAME_REJECTED}`;
+      const readStream = getFileStream(objectKey, bucket);
+      res.attachment(`${objectKey}.xlsx`);
       readStream.pipe(res);
     } catch (error) {
       console.error(error);
