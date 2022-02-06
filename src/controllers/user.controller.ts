11525f33a816a901d833c2a4e66a3e614946b3b6
Arjun Aghera <arjun.199301134@muj.manipal.edu>
2022-02-06 20:57:01 +0530

    fix: solved kpidata error in sheet controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,15 +5,8 @@ import { asyncWrap } from '../middlewares/async.middleware';
 import { User } from '../models/User.model';
 
 const createUser = async (data) => {
-  const {
-    firstName,
-    lastName,
-    email,
-    password,
-    details,
-    phoneNumber,
-    role,
-  } = data;
+  const { firstName, lastName, email, password, details, phoneNumber, role } =
+    data;
 
   // ============================================================
   // Validation
@@ -22,7 +15,10 @@ const createUser = async (data) => {
     throwError(400, 'Please provide all the information');
 
   const hashedPassword = await hash(password, 10);
-  const is_admin = (role.includes('admin') || role.includes('Admin') || role.includes('ADMIN')) ? true : false;
+  const is_admin =
+    role.includes('admin') || role.includes('Admin') || role.includes('ADMIN')
+      ? true
+      : false;
   return User.create({
     first_name: firstName,
     last_name: lastName,
@@ -33,17 +29,27 @@ const createUser = async (data) => {
     role,
     is_admin,
   });
-}
+};
 
 const updateUser = async (existingEmail, data) => {
-  const { details = null, ...rest } = data
+  const { details = null, ...rest } = data;
   const user = await User.findOne({ where: { email: existingEmail } });
-  if (!user) return throwError(400, "User Not Found");
+  if (!user) return throwError(400, 'User Not Found');
   const { password, ...restUserDetails } = user;
-  const updatedUser = { ...restUserDetails, ...rest, password, details: user?.details ? { ...user.details, ...details } : { ...details } };
-  const is_admin = (updatedUser.role.includes('admin') || updatedUser.role.includes('Admin') || updatedUser.role.includes('ADMIN')) ? true : false;
+  const updatedUser = {
+    ...restUserDetails,
+    ...rest,
+    password,
+    details: user?.details ? { ...user.details, ...details } : { ...details },
+  };
+  const is_admin =
+    updatedUser.role.includes('admin') ||
+    updatedUser.role.includes('Admin') ||
+    updatedUser.role.includes('ADMIN')
+      ? true
+      : false;
   return User.update({ email: existingEmail }, { ...updatedUser, is_admin });
-}
+};
 
 export const postAddUser: RequestHandler<any> = asyncWrap(
   async (req: Request, res: Response) => {
@@ -60,23 +66,38 @@ export const postAddUser: RequestHandler<any> = asyncWrap(
 
 export const getAllUser: RequestHandler<any> = asyncWrap(async (_req, res) => {
   try {
-    const data = await User.find({ select: ['id', 'first_name', 'email', 'last_name', 'role'] });
+    const data = await User.find({
+      select: ['id', 'first_name', 'email', 'last_name', 'role'],
+    });
     res.status(200).json(data);
   } catch (error) {
-    throwError(401, "Some error occured");
+    throwError(401, 'Some error occured');
   }
 });
 
-export const getSingleUser: RequestHandler<any> = asyncWrap(async (req, res) => {
-  try {
-    const { email } = req.params;
-    const data = await User.find({ select: ['id', 'first_name', 'email', 'last_name', 'phone_number', 'details', 'is_admin', 'role'], where: { email } });
-    res.status(200).json(data);
-  } catch (error) {
-    throwError(404, "User does not exist");
-  }
-});
-
+export const getSingleUser: RequestHandler<any> = asyncWrap(
+  async (req, res) => {
+    try {
+      const { email } = req.params;
+      const data = await User.find({
+        select: [
+          'id',
+          'first_name',
+          'email',
+          'last_name',
+          'phone_number',
+          'details',
+          'is_admin',
+          'role',
+        ],
+        where: { email },
+      });
+      res.status(200).json(data);
+    } catch (error) {
+      throwError(404, 'User does not exist');
+    }
+  },
+);
 
 export const putUpdateUser: RequestHandler<any> = asyncWrap(
   async (req: Request, res: Response) => {
@@ -88,56 +109,67 @@ export const putUpdateUser: RequestHandler<any> = asyncWrap(
       console.error(error);
       throwError(404, 'User does not exist');
     }
-  }
+  },
 );
-
 
 export const deleteUser: RequestHandler<any> = asyncWrap(
   async (req: Request, res: Response) => {
     try {
-      const { email } = req.params
+      const { email } = req.params;
       const user = await User.delete({ email });
       res.status(201).json(user);
     } catch (error) {
       console.error(error);
       throwError(404, 'User not found');
     }
-  }
+  },
 );
-
 
 export const getMyProfile: RequestHandler<any> = asyncWrap(
   async (req: Request, res: Response) => {
     try {
       const user: any = req.user;
-      if (!user) throwError(401, 'You are not logged in')
-      const data = await User.find({ select: ['first_name', 'email', 'last_name', 'phone_number', 'details'], where: { id: user.id } });
+      if (!user) throwError(401, 'You are not logged in');
+      const data = await User.find({
+        select: ['first_name', 'email', 'last_name', 'phone_number', 'details'],
+        where: { id: user.id },
+      });
       res.status(200).json(data);
     } catch (error) {
-      throwError(404, "User does not exist");
+      throwError(404, 'User does not exist');
     }
-  }
+  },
 );
-
 
 export const changeMyPassword: RequestHandler<any> = asyncWrap(
   async (req: Request, res: Response) => {
     try {
       const { oldPassword, newPassword, confirmPassword } = req.body;
       const user: any = req.user;
-      if (!oldPassword || !newPassword || !confirmPassword) throwError(406, "Please provide all the fields.");
-      if (!(newPassword === confirmPassword)) throwError(400, "Given new passwords didn't match")
+      if (!oldPassword || !newPassword || !confirmPassword)
+        throwError(406, 'Please provide all the fields.');
+      if (!(newPassword === confirmPassword))
+        throwError(400, "Given new passwords didn't match");
       const hashedPassword = await hash(newPassword, 10);
-      const userData = User.find({ select: ['password'], where: { id: user.id } })
-      compare(oldPassword, userData[0].password, async (err, result: boolean) => {
-        if (err) throw err;
-        if (!result) throwError(404, "Given Password is wrong");
-        const data = await User.update({ id: user.id }, { password: hashedPassword });
-        res.status(200).json(data);
+      const userData = User.find({
+        select: ['password'],
+        where: { id: user.id },
       });
+      compare(
+        oldPassword,
+        userData[0].password,
+        async (err, result: boolean) => {
+          if (err) throw err;
+          if (!result) throwError(404, 'Given Password is wrong');
+          const data = await User.update(
+            { id: user.id },
+            { password: hashedPassword },
+          );
+          res.status(200).json(data);
+        },
+      );
     } catch (error) {
-      throwError(404, "User does not exist");
+      throwError(404, 'User does not exist');
     }
-  }
+  },
 );
-
