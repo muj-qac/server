d8c5f415e0e2fee454c8c03b14f40e26b58d9006
Arjun Aghera <arjun.199301134@muj.manipal.edu>
2022-02-08 20:31:05 +0530

    fix:sheet download route to user route and added a check to not update verified kpi

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,6 +3,7 @@ import * as user from '../controllers/user.controller';
 import * as kpi from '../controllers/kpi.controller';
 import { postKPI } from '../controllers/upload.controller';
 import uploadMiddleware from '../middlewares/upload.middleware';
+import { downloadSheet } from '../controllers/sheet.controller';
 
 const router = Router();
 
@@ -15,6 +16,8 @@ router.get('/', (req, res) => {
 router.put('/change-password', user.changeMyPassword);
 
 router.get('/alloted-kpi', kpi.getAllocatedKpi);
+
+router.get('/download/:id', downloadSheet);
 
 router.post('/upload/:kpiId', uploadMiddleware.single('file'), postKPI);
 
