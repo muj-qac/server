3eb8e10e97328d98dae61978679453149b57276b
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2022-06-26 23:11:13 +0530

    fix: getVerifiedKPIs orWhere

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -356,7 +356,8 @@ export const getVerifiedKPIs: RequestHandler<any> = asyncWrap(
         'uploaded_sheets',
       )
         .leftJoinAndSelect('uploaded_sheets.user', 'user_id')
-        .where({ status: statusTypes.VERIFIED && statusTypes.MERGED })
+        .where({ status: statusTypes.VERIFIED })
+        .orWhere({ status: statusTypes.MERGED })
         .execute();
 
       if (!verifiedKpis) throwError(500, 'No Entry in Database');
