e22dd225c60418f801dbb29135b6048dd0a8802b
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2022-06-27 00:05:46 +0530

    update: new admin email

--- src/constants/sheet.ts
+++ src/constants/sheet.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_SHEET_ROWS = 2000;
 export const SERVICE_ACCOUNT_EMAIL =
-  'qnc-service-account@qnc-muj.iam.gserviceaccount.com';
-export const ADMIN_ACCOUNT_EMAIL = 'qncmuj@gmail.com';
+  'qnc-sheets-creation@qnc-sheets.iam.gserviceaccount.com';
+export const ADMIN_ACCOUNT_EMAIL = 'qncportalmuj@gmail.com';
